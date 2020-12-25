017574bdab9cf89387596e0ed79e1f1d9cd39039
Surface <sakurai@is-jpn.com>
2020-12-25 12:53:07 +0900

    page

--- DjangoSearchView/sample/forms.py
+++ DjangoSearchView/sample/forms.py
@@ -3,6 +3,7 @@ Definition of forms.
 """
 
 from django import forms
+from django.forms.widgets import DateInput
 from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import ugettext_lazy as _
 
@@ -16,3 +17,61 @@ class BootstrapAuthenticationForm(AuthenticationForm):
                                widget=forms.PasswordInput({
                                    'class': 'form-control',
                                    'placeholder':'Password'}))
+
+# Test For SearchView
+from searchview.forms import SearchForm
+
+class MusicianSearchForm(SearchForm):
+    """Search for Musicians."""
+
+    first_name = forms.CharField(
+        label='first_name =',
+        required = False,
+        max_length=50,
+    )
+    last_name__contains = forms.CharField(
+        label="lasst_name like '%val%'",
+        required = False,
+        max_length=50,
+    )
+    instrument__istartswith = forms.CharField(
+        label="instrument like 'val%'",
+        required = False,
+        max_length=32,
+    )
+    album_count__gte = forms.IntegerField(
+        label='album_count >=',
+        required = False,
+        min_value=0,
+        max_value=999,
+    )
+    album_count__lt = forms.IntegerField(
+        label='album_count <',
+        required = False,
+        min_value=0,
+        max_value=999,
+    )
+
+class AlbumSearchForm(SearchForm):
+    """Search for Albums."""
+
+    name__istartswith = forms.CharField(
+        label="name like 'val%'",
+        required = False,
+        max_length=100,
+    )
+    release_date__gte = forms.DateField(
+        label='release_date >=',
+        required = False,
+        widget=DateInput(),
+    )
+    release_date__lte = forms.DateField(
+        label='release_date <=',
+        required = False,
+        widget=DateInput(),
+    )
+    artist__first_name__contains = forms.CharField(
+        label="Musician.first_name like '%val%'",
+        required = False,
+        max_length=50,
+    )
--- DjangoSearchView/sample/models.py
+++ DjangoSearchView/sample/models.py
@@ -5,3 +5,15 @@ Definition of models.
 from django.db import models
 
 # Create your models here.
+
+# Test For SearchView
+class Musician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=100)
+
+class Album(models.Model):
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    num_stars = models.IntegerField()
