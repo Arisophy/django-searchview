470137b1439e7e39f234ec045c7ea9fc820e07b7
Arisophy <sakurai@is-jpn.com>
2021-12-21 17:55:55 +0900

    Django4.0

--- DjangoSearchView/sample/forms.py
+++ DjangoSearchView/sample/forms.py
@@ -5,18 +5,7 @@ Definition of forms.
 from django import forms
 from django.forms.widgets import DateInput
 from django.contrib.auth.forms import AuthenticationForm
-from django.utils.translation import ugettext_lazy as _
 
-class BootstrapAuthenticationForm(AuthenticationForm):
-    """Authentication form which uses boostrap CSS."""
-    username = forms.CharField(max_length=254,
-                               widget=forms.TextInput({
-                                   'class': 'form-control',
-                                   'placeholder': 'User name'}))
-    password = forms.CharField(label=_("Password"),
-                               widget=forms.PasswordInput({
-                                   'class': 'form-control',
-                                   'placeholder':'Password'}))
 
 # Test For SearchView
 from searchview.forms import SearchForm
